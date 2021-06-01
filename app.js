5a553fa5bae53122f74ad851fb5530934a1652a4
alishkeir <alishkeir_@hotmail.com>
2021-06-01 21:52:11 +0300

    handling middlewares

--- app.js
+++ app.js
@@ -2,15 +2,22 @@ const express = require("express");
 
 const app = express();
 
+// Middlewares
+
+app.use((req, res, next) => {
+  console.log(req.ip);
+  next();
+});
+
 // Routes
 app.get("/", (req, res, next) => {
   res.send("Welcome to home page");
 });
 
 app.get("/user/:id/:postId", (req, res, next) => {
-  console.log(req.params);
-  console.log(req.get("Host"));
-  console.log(req.query);
+//   console.log(req.params);
+//   console.log(req.get("Host"));
+//   console.log(req.query);
   res.send("User Page");
 });
 
