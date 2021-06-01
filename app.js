5201f5652ee2a4c5c366e54ff5fc023e8cad2210
alishkeir <alishkeir_@hotmail.com>
2021-06-01 21:44:55 +0300

    Handling Routes

--- app.js
+++ app.js
@@ -2,4 +2,16 @@ const express = require("express");
 
 const app = express();
 
+// Routes
+app.get("/", (req, res, next) => {
+  res.send("Welcome to home page");
+});
+
+app.get("/user/:id/:postId", (req, res, next) => {
+  console.log(req.params);
+  console.log(req.get("Host"));
+  console.log(req.query);
+  res.send("User Page");
+});
+
 module.exports = app;
